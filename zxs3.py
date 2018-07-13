c9bdf6e60f4a701ffc7a9a717156e090f8493198
zhangwj8517 <zhangwj8517@163.com>
2018-07-13 09:11:04 +0800

    Update zxs3.py
    update: solved  load configure info error

--- zxs3.py
+++ zxs3.py
@@ -2,123 +2,202 @@ import requests
 import datetime
 import time
 import configparser
+import logging
+
+logger = logging.getLogger("booking seat")
+logger.setLevel(logging.DEBUG)
+ch = logging.StreamHandler()
+ch.setLevel(logging.DEBUG)
+formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+ch.setFormatter(formatter)
+# 将相应的handler添加在logger对象中
+logger.addHandler(ch)
 
 cf = configparser.ConfigParser()
 cf.read('booking.conf')
 
 START_TIME = cf.get("time", "START_TIME")
 END_TIME = cf.get("time", "END_TIME")
-goodSeatNo = cf.get("seatnos", "goodSeatNo")
-userinfo1 = cf.get("users", "userinfo1")
-userinfo2 = cf.get("users", "userinfo2")
-IP = cf.get("IP_PORT", "IP")
-PORT = cf.get("IP_PORT", "PORT")
-url = 'http://' + IP + ':' + PORT
+url = cf.get("HTTP_INFO", "url")
 
-print(START_TIME)
-print(END_TIME)
-print(goodSeatNo)
-print(userinfo1)
-print(userinfo2)
-print(url)
+# 从配置中读取string类型的位置信息，转换成list类型并strip每个元素
+str_goodSeatNo = cf.get("seatnos", "goodSeatNo")
+goodSeatNo = []
+for j in str_goodSeatNo.split(','):
+    goodSeatNo.append(j.strip())
+
+alluserinfo = []
+for section in cf.sections():
+    if "userinfo" in section:
+        alluserinfo.append(dict(cf.items(section)))
+
+logger.info("START_TIME: " + START_TIME)
+logger.info("END_TIME: " + END_TIME)
+logger.info("url: " + url)
+logger.info("good SeatNo list: " + str(goodSeatNo))
+logger.debug("all user info: " + str(alluserinfo))
 
 
-def check_position(user_info):
+# get 阅览室id :yls_id
+def get_ysl_id():
     try:
-        # get 阅览室id :yls_id
+        yls_id = ''
         response = requests.post(url + "/seat/yuelanshi_list?mode=local")
-        serial_no = response.json()["serialno"]
+        if "排队成功" in response.json()["content"]:
+            serial_no = response.json()["serialno"]
+        else:
+            logger.debug(str(response.json()["content"]))
+            return yls_id
+
+        # time.sleep(1)
         response = requests.post(url + "/seat/get_task_status?serialno=" + serial_no)
-        if response.json()["content"] == "没有可以预定的阅览室.":
-            print(response.json()["content"])
-            return False
+        if "没有可以预定的阅览室." in response.json()["content"]:
+            logger.info(str(response.json()["content"]))
+            return yls_id
         else:
             # 获取阅览室ID
-            if "排队中请等待." in response.json()["content"]:
-                print(response.json()["content"])
-                return False
+            # if "排队中请等待." in response.json()["content"]:
+            #     return yls_id
+            i = 0
+            while "排队中请等待" in response.json()["content"]:
+                logger.debug("正在排队中: " + str(response.json()["content"]))
+                response = requests.post(url + "/seat/get_task_status?serialno=" + serial_no)
+                time.sleep(1)
+                i = i + 1
+                if i >= 5:
+                    return yls_id
 
+            logger.debug("获取阅览室ID: " + str(response.json()["content"]))
             yls_id = response.json()["content"][0]["id"]
+            logger.info("阅览室ID: " + str(yls_id))
+            return yls_id
+    except Exception as e:
+        print("Exception: ", e)
+        return yls_id
 
-            print("阅览室ID", yls_id)
-            # 获取阅览室可订位置列表，并获取排队序号
-            response = requests.get(url + '/seat/yuelanshi_seat?mode=local&yuelanshiId=' + yls_id)
-            ser_no2 = response.json()["serialno"]
-            print("排序号: ", ser_no2)
 
-            # 根据当前的排队序号，查看还有多少可选的位置
-            response = requests.get(url + '/seat/get_task_status?serialno=' + ser_no2)
-            if (response.json()["content"]) == "没有座位.":
-                print("没有座位.")
+# 选定位置
+def check_position(user_info, yls_id):
+    try:
+        # 获取阅览室可订位置列表，并获取排队序号
+        response = requests.post(url + '/seat/yuelanshi_seat?mode=local&yuelanshiId=' + yls_id)
+        ser_no2 = response.json()["serialno"]
+        logger.debug("排序号: " + str(ser_no2))
+        # 根据当前的排队序号，查看还有多少可选的位置
+        # time.sleep(1)
+        logger.info("查看可选位置......")
+        response = requests.post(url + '/seat/get_task_status?serialno=' + ser_no2)
+
+        st = time.time()
+        while "没有座位." in response.json()["content"]:
+            response = requests.post(url + '/seat/get_task_status?serialno=' + ser_no2)
+            ed = time.time()
+            # 持续200秒后退出
+            if (ed - st) >= 30:
+                logger.info("没有座位.")
                 return False
+
+        i = 0
+        while "排队中" in response.json()["content"]:
+            logger.debug("getting seat no : " + str(response.json()["content"]))
+            response = requests.post(url + '/seat/get_task_status?serialno=' + ser_no2)
+            time.sleep(1)
+            i = i + 1
+            if i >= 5:
+                return False
+
+        logger.debug(str(response.json()["content"]))
+        logger.info("remain seat num: " + str(len(response.json()["content"])))
+        # 空着的位置有哪些
+        remain_seats = []
+        for i in range(0, len(response.json()["content"])):
+            remain_seats.append(response.json()["content"][i]["seatno"])
+
+        logger.debug("type([0]seat_no): " + type(response.json()["content"][0]["seatno"]))
+        logger.debug("all seat no: " + str(remain_seats))
+        # 最想要的几个位置还空着的话，就按先后顺序选最想要的位置
+        seat_no = '0'
+        for g in goodSeatNo:
+            if (g in remain_seats) or (int(g) in remain_seats):
+                seat_no = g
+                logger.debug("str(type(g)): " + type(g))
+                break
+        # 没有选上中意的位置时，取剩余的中间位置
+        if '0' == seat_no:
+            middle = int(len(response.json()["content"]) / 2)
+            seat_no = response.json()["content"][middle]["seatno"]
+
+        # 根据阅览室id,选取的位置seat_no,帐户信息进行预定
+        logger.info("select seatNo: " + str(seat_no))
+        my_data = {'yuelanshiId': yls_id, 'seatNo': seat_no}
+        my_data.update(user_info)
+        logger.debug(url + '/seat/orderMySeat?mode=local' + str(my_data))
+        response1 = requests.post(url + '/seat/orderMySeat?mode=local', my_data)
+        i = 0
+        while "排队中" in response1.json()["content"]:
+            response1 = requests.post(url + '/seat/orderMySeat?mode=local', my_data)
+            time.sleep(1)
+            i = i + 1
+            if i >= 5:
+                return False
+
+        # 排队成功后，验证结果，查看是否预定成功
+        logger.debug(str(response1.json()["content"]))
+        if "排队成功！" in response1.json()["content"]:
+            check_serialno = response1.json()["serialno"]
+            headers = {"Referer": url + "/seat/XueShengloginByNo.jsp?yid=" + str(
+                yls_id) + "&seatno=" + str(seat_no)}
+            logger.debug("headers info :  " + str(headers))
+            time.sleep(1)
+            response2 = requests.post(
+                url + '/seat/get_task_status?serialno=' + str(check_serialno),
+                headers=headers)
+            i = 0
+            while "排队中" in response2.json()["content"]:
+                response2 = requests.post(
+                    url + '/seat/get_task_status?serialno=' + str(check_serialno),
+                    headers=headers)
+                time.sleep(1)
+                i = i + 1
+                if i >= 5:
+                    return False
+
+            # 根据返回信息情况，判断本次是否预定上，或是之前已经预定上
+            logger.debug(str(response2.json()["content"]))
+            booking_sd = response2.json()["content"]
+            if ("之前到终端机刷卡" in booking_sd) or ("不可以连续预定." in booking_sd):
+                return True
             else:
-                print("remain seat nums: ", len(response.json()["content"]))
-                # 空着的位置有哪些
-                remain_seats = []
-                for i in range(0, len(response.json()["content"])):
-                    remain_seats.append(response.json()["content"][i]["seatno"])
-                    # print("all seat no:", response.json()["content"][i]["seatno"])
-                print("all seat no: ", str(remain_seats))
-                seat_no = '0'
-                # 最想要的几个位置还空着的话，就按先后顺序选最想要的位置
-                for g in goodSeatNo:
-                    if (str(g) in remain_seats) or (g in remain_seats):
-                        seat_no = str(g)
-                        break
-                # 没有选上中意的位置时，取剩余的中间位置
-                if seat_no == '0':
-                    middle = int(len(response.json()["content"]) / 2)
-                    seat_no = response.json()["content"][middle]["seatno"]
-
-                # 根据阅览室id,选取的位置，帐户信息排队并预定位置
-                my_data = {'yuelanshiId': yls_id, 'seatNo': seat_no}
-                my_data.update(user_info)
-                response1 = requests.post(url + '/seat/orderMySeat?mode=local', my_data)
-                print(response1.json()["content"])
-
-                # 排队成功后，验证结果，查看是否预定成功
-                if "排队成功！" == response1.json()["content"]:
-                    check_serialno = response1.json()["serialno"]
-                    print("select seatNo: ", seat_no)
-                    headers = {
-                        "Referer": url + "/seat/XueShengloginByNo.jsp?yid=" + str(
-                            yls_id) + "&seatno=" + str(
-                            seat_no)}
-                    print("headers info :  ", headers)
-                    time.sleep(1)
-                    response2 = requests.post(
-                        url + '/seat/get_task_status?serialno=' + str(check_serialno),
-                        headers=headers)
-
-                    # 根据返回信息情况，判断本次是否预定上，或是之前已经预定上
-                    print(response2.json()["content"])
-                    booking_sd = response2.json()["content"]
-                    if ("之前到终端机刷卡" in booking_sd) or ("不可以连续预定." in booking_sd):
-                        return True
-                    else:
-                        return False
+                return False
     except Exception as e:
         print("Exception: ", e)
         return False
 
 
-def book_position(user_info):
-    NOW_TIME = datetime.datetime.now().time().strftime("%H:%M:%S")
+def book_position():
+    # 在指定时间内才开始预定
+    now_time = datetime.datetime.now().time().strftime("%H:%M:%S")
+    while END_TIME < now_time < START_TIME:
+        logger.info("booking time is not coming, waiting 30 s ...... ")
+        time.sleep(30)
+        now_time = datetime.datetime.now().time().strftime("%H:%M:%S")
+
+    logger.info("start booking ......")
+    # get ysl id
     while True:
-        if END_TIME < NOW_TIME < START_TIME:
-            print("booking time is not coming, waiting 30 s ...... ")
-            time.sleep(30)
-            NOW_TIME = datetime.datetime.now().time().strftime("%H:%M:%S")
-        else:
-            print("start booking ......")
-            if check_position(user_info):
-                print("Book success !")
+        ysl_id = get_ysl_id()
+        if len(ysl_id) > 0:
+            break
+
+    # for every user book seat
+    for user_info in alluserinfo:
+        while True:
+            if check_position(user_info, ysl_id):
+                logger.info("Book success !")
                 break
             else:
-                # print("book fail, wait 10 s ......")
-                time.sleep(5)
+                pass
 
 
-book_position(userinfo1)
-print("next persion:........")
-book_position(userinfo2)
+# main function
+book_position()
