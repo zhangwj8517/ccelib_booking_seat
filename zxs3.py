2be6a5adad10d7b6d1c517b39da6e21021566a9a
zhangwj8517 <zhangwj8517@163.com>
2018-07-17 21:06:37 +0800

    Update zxs3.py

--- zxs3.py
+++ zxs3.py
@@ -3,6 +3,7 @@ import datetime
 import time
 import configparser
 import logging
+from multiprocessing import Pool
 
 logger = logging.getLogger("booking seat")
 logger.setLevel(logging.DEBUG)
@@ -49,24 +50,12 @@ def get_ysl_id():
             logger.debug(str(response.json()["content"]))
             return yls_id
 
-        # time.sleep(1)
         response = requests.post(url + "/seat/get_task_status?serialno=" + serial_no)
-        if "没有可以预定的阅览室." in response.json()["content"]:
+        content = response.json()["content"]
+        if ("没有可以预定的阅览室" in content) or ("排队中请等待" in content):
             logger.info(str(response.json()["content"]))
             return yls_id
         else:
-            # 获取阅览室ID
-            # if "排队中请等待." in response.json()["content"]:
-            #     return yls_id
-            i = 0
-            while "排队中请等待" in response.json()["content"]:
-                logger.debug("正在排队中: " + str(response.json()["content"]))
-                response = requests.post(url + "/seat/get_task_status?serialno=" + serial_no)
-                time.sleep(1)
-                i = i + 1
-                if i >= 5:
-                    return yls_id
-
             logger.debug("获取阅览室ID: " + str(response.json()["content"]))
             yls_id = response.json()["content"][0]["id"]
             logger.info("阅览室ID: " + str(yls_id))
@@ -84,43 +73,29 @@ def check_position(user_info, yls_id):
         ser_no2 = response.json()["serialno"]
         logger.debug("排序号: " + str(ser_no2))
         # 根据当前的排队序号，查看还有多少可选的位置
-        # time.sleep(1)
-        logger.info("查看可选位置......")
+        # logger.info("查看可选位置......")
         response = requests.post(url + '/seat/get_task_status?serialno=' + ser_no2)
-
-        st = time.time()
-        while "没有座位." in response.json()["content"]:
-            response = requests.post(url + '/seat/get_task_status?serialno=' + ser_no2)
-            ed = time.time()
-            # 持续200秒后退出
-            if (ed - st) >= 30:
-                logger.info("没有座位.")
-                return False
-
-        i = 0
-        while "排队中" in response.json()["content"]:
-            logger.debug("getting seat no : " + str(response.json()["content"]))
-            response = requests.post(url + '/seat/get_task_status?serialno=' + ser_no2)
-            time.sleep(1)
-            i = i + 1
-            if i >= 5:
-                return False
-
         logger.debug(str(response.json()["content"]))
+        content = response.json()["content"]
+        if ("没有座位" in content) or ("排队中" in content):
+            logger.info(str(response.json()["content"]))
+            return False
+
         logger.info("remain seat num: " + str(len(response.json()["content"])))
+        logger.info("remain seat info: " + str(response.json()["content"]))
         # 空着的位置有哪些
         remain_seats = []
         for i in range(0, len(response.json()["content"])):
             remain_seats.append(response.json()["content"][i]["seatno"])
 
-        logger.debug("type([0]seat_no): " + type(response.json()["content"][0]["seatno"]))
+        logger.debug("type([0]seat_no): " + str(type(response.json()["content"][0]["seatno"])))
         logger.debug("all seat no: " + str(remain_seats))
         # 最想要的几个位置还空着的话，就按先后顺序选最想要的位置
         seat_no = '0'
         for g in goodSeatNo:
             if (g in remain_seats) or (int(g) in remain_seats):
                 seat_no = g
-                logger.debug("str(type(g)): " + type(g))
+                logger.debug("str(type(g)): " + str(type(g)))
                 break
         # 没有选上中意的位置时，取剩余的中间位置
         if '0' == seat_no:
@@ -133,13 +108,6 @@ def check_position(user_info, yls_id):
         my_data.update(user_info)
         logger.debug(url + '/seat/orderMySeat?mode=local' + str(my_data))
         response1 = requests.post(url + '/seat/orderMySeat?mode=local', my_data)
-        i = 0
-        while "排队中" in response1.json()["content"]:
-            response1 = requests.post(url + '/seat/orderMySeat?mode=local', my_data)
-            time.sleep(1)
-            i = i + 1
-            if i >= 5:
-                return False
 
         # 排队成功后，验证结果，查看是否预定成功
         logger.debug(str(response1.json()["content"]))
@@ -148,21 +116,12 @@ def check_position(user_info, yls_id):
             headers = {"Referer": url + "/seat/XueShengloginByNo.jsp?yid=" + str(
                 yls_id) + "&seatno=" + str(seat_no)}
             logger.debug("headers info :  " + str(headers))
+
+            # 1秒后，根据返回序列号及之前提交的位置信息，判断本次是否预定上，或是之前已经预定上
             time.sleep(1)
             response2 = requests.post(
                 url + '/seat/get_task_status?serialno=' + str(check_serialno),
                 headers=headers)
-            i = 0
-            while "排队中" in response2.json()["content"]:
-                response2 = requests.post(
-                    url + '/seat/get_task_status?serialno=' + str(check_serialno),
-                    headers=headers)
-                time.sleep(1)
-                i = i + 1
-                if i >= 5:
-                    return False
-
-            # 根据返回信息情况，判断本次是否预定上，或是之前已经预定上
             logger.debug(str(response2.json()["content"]))
             booking_sd = response2.json()["content"]
             if ("之前到终端机刷卡" in booking_sd) or ("不可以连续预定." in booking_sd):
@@ -199,5 +158,12 @@ def book_position():
                 pass
 
 
-# main function
 book_position()
+# # main function
+# p_cnt = 1
+# if __name__ == '__main__':
+#     p = Pool(p_cnt)
+#     for i in range(p_cnt):
+#         p.apply_async(book_position)
+#     p.close()
+#     p.join()
